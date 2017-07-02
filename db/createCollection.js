2ff800deef10bba76a7b6b749583ceffa1e7b3a2
Ray Dollete <ray@raydollete.com>
2017-07-01 23:16:43 -0700

    Update references in createCollection to Google Datastore

--- db/createCollection.js
+++ db/createCollection.js
@@ -1,7 +1,12 @@
+var DatastoreClient = require("@google-cloud/datastore")({
+	projectId: 'crypto-bigquery',
+	keyFilename: '/home/allenday/crypto-bigquery.json'
+});
+
 module.exports = function container (get, set) {
   return function createCollection (name, options) {
-    return require('sosa_mongo')({
-      db: get('db.mongo')
+    return require('../../sosa_datastore/')({
+      db: DatastoreClient
     })(name, options)
   }
-}
+};
